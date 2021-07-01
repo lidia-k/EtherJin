5cd6e976e522393d3ec5d191c88ac7320f7f8208
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-01 11:32:18 -0600

    updated the models; updated the views to save address and transactions into the models

--- etherscan_app/models.py
+++ etherscan_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class Address(models.Model):
-    address = models.CharField(max_length=50)
-
+    address = models.CharField(max_length=50, unique=True)
 class Transaction(models.Model):
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, related_name='transactions', on_delete=models.CASCADE)
     from_account = models.CharField(max_length=50)
     to_account = models.CharField(max_length=50)
-    value = models.BigIntegerField()
+    value_in_ether = models.IntegerField(default=0)
--- etherscan_app/views.py
+++ etherscan_app/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from etherscan_app.utils import validate_address
 from etherscan_app.models import Address, Transaction
+from django_q.tasks import async_task
 
 def index(request):
     return render(request, 'etherscan_app/index.html')
 
 def search(request):
     return render(request, 'etherscan_app/search.html')
+
+def create_transaction(transactions):
+    Transaction.objects.bulk_create(transactions)
 
 def create_address(request):
     address = request.POST.get("address")
@@ -16,8 +20,10 @@ def create_address(request):
     
     if valid_address:
         address_instance = Address.objects.create(address=address)
+        transactions = []
         for i in range(len(transaction_data)):
             transaction = transaction_data[i]
-            transaction_instance = Transaction.objects.create(address=address_instance, from_account=transaction['from'], to_account=transaction['to'], value=transaction['value'])
-
+            transaction_instance = Transaction(address=address_instance, from_account=transaction['from'], to_account=transaction['to'], value_in_ether=int(transaction['value'])/1e18)
+            transactions.append(transaction_instance)
+        async_task('etherscan_app.views.create_transaction', transactions)
     return HttpResponse(f'message: {response_data["message"]}, result: {response_data["result"]}')
