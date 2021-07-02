2f82dba2dea4f730815d9e3b9840b8357b3db552
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-02 12:55:57 -0600

    create tests

--- etherscan_app/tests.py
+++ etherscan_app/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from etherscan_app.utils import validate_address, create_address
+from etherscan_app.models import Address
+from etherscan_app.views import show_results
+from django.db.utils import IntegrityError 
+from django.urls import reverse
 
-# Create your tests here.
+class ValidateAddressTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('results') 
+
+    def test_validate_address_with_valid_address(self):
+        """
+        validate_address() returns True for a valid address 
+        """
+        address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+        valid_address, response_data = validate_address(address)
+        self.assertTrue(valid_address)
+
+    def test_validate_address_with_invalid_address(self):
+        """
+        validate_address() returns False for an invalid address
+        """
+        address = "1234567890aaazzz"
+        valid_address, response_data = validate_address(address)
+        response = self.client.post(self.url, {'address': address})
+        self.assertFalse(valid_address)
+        self.assertEqual(response.status_code, 400)
+class CreateAddressTests(TestCase):
+    def setUp(self):
+        self.address = "0xD4fa6E82c77716FA1EF7f5dEFc5Fd6eeeFBD3bfF"
+        self.address_instance = Address.objects.create(address=self.address)
+    
+    def test_create_address_with_valid_address(self):
+        new_address = '0x8d7c9AE01050a31972ADAaFaE1A4D682F0f5a5Ca'
+        create_address(new_address)
+        self.assertEqual(Address.objects.last().address, new_address)
+
+    def test_create_address_with_already_existing_address(self):
+        address_instance = create_address(self.address)
+        self.assertEqual(address_instance, None)
