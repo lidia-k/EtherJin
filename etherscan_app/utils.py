8b7c8ade8a8d141f3b114a61f75461aca7cb4292
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-02 11:43:56 -0600

    add error handling for invalid api_token

--- etherscan_app/utils.py
+++ etherscan_app/utils.py
@@ -2,6 +2,8 @@ import os
 import requests
 import json
 from ratelimiter import RateLimiter
+from etherscan_app.models import Address, Transaction
+import sys
 
 @RateLimiter(max_calls=5, period=1)
 def validate_address(address):
@@ -12,10 +14,30 @@ def validate_address(address):
     dict (response json)
     """
     api_token = os.environ.get("API_TOKEN")
+    if not api_token:
+        sys.exit("Please provide api_token.") 
+
     etherscan_api = f'https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock=0&endblock=99999999&sort=asc&apikey={api_token}'
-    
     response = requests.get(etherscan_api)
     response_data = json.loads(response.text)
     valid_address = response_data["status"] == "1" and response_data["message"] == "OK"
-    return (valid_address, response_data)
     
+    return (valid_address, response_data)
+
+def create_address(address):  
+    """
+    Creates an address instance for a valid address 
+    """
+    address_instance = Address.objects.create(address=address)
+    return address_instance
+
+def create_transaction(address_instance, result_data):
+    """
+    Populate transaction data of a valid address 
+    """
+    transactions = []
+    for i in range(len(result_data)):
+        transaction = result_data[i]
+        transaction_instance = Transaction(address=address_instance, from_account=transaction['from'], to_account=transaction['to'], value_in_ether=int(transaction['value'])/1e18)
+        transactions.append(transaction_instance)
+    Transaction.objects.bulk_create(transactions)
--- etherscan_app/views.py
+++ etherscan_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from etherscan_app.utils import validate_address
+from etherscan_app.utils import validate_address, create_address, create_transaction
 from etherscan_app.models import Address, Transaction
 from django_q.tasks import async_task
 
@@ -10,25 +10,15 @@ def index(request):
 def search(request):
     return render(request, 'etherscan_app/search.html')
 
-def create_transaction(address_instance, transaction_data):
-    transactions = []
-    for i in range(len(transaction_data)):
-        transaction = transaction_data[i]
-        transaction_instance = Transaction(address=address_instance, from_account=transaction['from'], to_account=transaction['to'], value_in_ether=int(transaction['value'])/1e18)
-        transactions.append(transaction_instance)
-    Transaction.objects.bulk_create(transactions)
-
-def create_address(address):  
-    address_instance = Address.objects.create(address=address)
-    return address_instance
-
 def show_results(request):
     address = request.POST.get("address")
     valid_address, response_data = validate_address(address)
-    transaction_data = response_data['result']
+    result_data = response_data['result']
     
     if valid_address:
         address_instance = create_address(address) 
-        async_task('etherscan_app.views.create_transaction', address_instance, transaction_data)
-    
-    return HttpResponse(f'message: {response_data["message"]}, result: {transaction_data}')
+        async_task('etherscan_app.utils.create_transaction', address_instance, result_data)
+        return HttpResponse(f'message: {response_data["message"]}, result: {result_data}')
+    elif result_data == 'Invalid API Key':
+        print("Please provide a valid api_token.")
+        return HttpResponse(status=500)
