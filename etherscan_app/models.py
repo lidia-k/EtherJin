e1bf26c16982ebd176a34d4ce3c773b761b4f413
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-02 18:32:54 -0600

    update the Transaction model to have hash

--- etherscan_app/models.py
+++ etherscan_app/models.py
@@ -4,6 +4,7 @@ class Address(models.Model):
     address = models.CharField(max_length=50, unique=True)
 class Transaction(models.Model):
     address = models.ForeignKey(Address, related_name='transactions', on_delete=models.CASCADE)
+    hash = models.CharField(max_length=200, unique=True, default='')
     from_account = models.CharField(max_length=50)
     to_account = models.CharField(max_length=50)
     value_in_ether = models.IntegerField(default=0)
