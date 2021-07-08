bf0c6a944b4d167ecd847cc0a2e4df198c5ec24c
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-07 18:47:10 -0600

    add created_at field for Address model

--- etherscan_app/models.py
+++ etherscan_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Address(models.Model):
     address = models.CharField(max_length=50, unique=True, primary_key=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
 class Transaction(models.Model):
     address = models.ForeignKey(Address, related_name='transactions', on_delete=models.CASCADE)
     hash = models.CharField(max_length=200, unique=True, primary_key=True)
