e8ece906266555585a6bb69671e2d4742868d845
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-13 21:47:10 -0500

    change users field of Address model to many-to-many relationships

--- etherscan_app/models.py
+++ etherscan_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Address(models.Model):
-    user = models.ForeignKey(User, related_name='address', on_delete=models.CASCADE, null=True)
+    users = models.ManyToManyField(User)
     address = models.CharField(max_length=50, unique=True, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
 class Transaction(models.Model):
