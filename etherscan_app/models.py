ee73d8aa8ada3e0b52017c7cc1a8436d2030ad80
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-16 17:42:23 -0500

    create Folder Model

--- etherscan_app/models.py
+++ etherscan_app/models.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+
+class Folder(models.Model):
+    folder_name = models.CharField(max_length=50)
 class Address(models.Model):
     users = models.ManyToManyField(User, related_name='addresses')
+    folders = models.ManyToManyField(Folder, related_name='folders')
     address = models.CharField(max_length=50, unique=True, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 class Transaction(models.Model):
     address = models.ForeignKey(Address, related_name='transactions', on_delete=models.CASCADE)
     hash = models.CharField(max_length=200, unique=True, primary_key=True)
