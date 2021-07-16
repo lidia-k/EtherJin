889aab996c33a20d86503758401beea2a4ffb94a
Hyunjin Kwon <lidia@Hyunjins-MacBook-Pro.local>
2021-07-16 17:17:03 -0500

    change the name of the template to login.html

--- django_social_app/views.py
+++ django_social_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect, render
 def login(request):
     if request.user.is_authenticated:
         return redirect('etherscan_app:index')
-    return render(request, 'django_social_app/home.html')
+    return render(request, 'django_social_app/login.html')
 
 def logout(request):
     auth_logout(request)
