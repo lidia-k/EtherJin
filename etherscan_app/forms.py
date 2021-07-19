accb866d20133f755088d68537fd6a4f76a33ff2
Hyunjin Kwon <lidiakwon212@gmail.com>
2021-07-19 13:16:51 -0500

    update the attribute name folder to folder_name

--- etherscan_app/forms.py
+++ etherscan_app/forms.py
@@ -8,7 +8,7 @@ class FolderSelectionForm(forms.Form):
 
     def __init__(self, folders, *args, **kwargs):
         super(FolderSelectionForm, self).__init__(*args, **kwargs)
-        self.fields['folder'].queryset = folders.values_list('folder', flat=True)
+        self.fields['folder'].queryset = folders.values_list('folder_name', flat=True)
 
 class FolderCreationFrom(forms.Form):
     folder = forms.CharField(label='Folder name', max_length=50)
