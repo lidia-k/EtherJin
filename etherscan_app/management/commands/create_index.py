144fd897859a6fb5dab7703b1ad92e4fc705a566
root <root@localhost>
2021-08-02 18:56:06 +0000

    add a management command to create es index; add the management command in the dockerfile

---
+++ etherscan_app/management/commands/create_index.py
@@ -0,0 +1,14 @@
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from etherscan_app.indexes import FolderDocument
+from elasticsearch_dsl import Index
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        index = Index(settings.ELASTICSEARCH_INDEX)
+        index.delete(ignore=404)
+        index.document(FolderDocument)
+        index.create()
+        self.stdout.write(self.style.SUCCESS(f'{index} index successfully created!'))
