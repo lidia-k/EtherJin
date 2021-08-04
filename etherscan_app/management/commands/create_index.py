2defcf25ffd9265eeb9ae7c78ef0df7832ec6337
root <root@localhost>
2021-08-04 22:52:56 +0000

    add ignore=404 as index.delete argument so when there is no index, it doesn't throw an error and stops the server; add a list comprehension to index all the folder model objects when creating an index

--- etherscan_app/management/commands/create_index.py
+++ etherscan_app/management/commands/create_index.py
@@ -4,14 +4,15 @@ from django.core.management.base import BaseCommand
 
 from elasticsearch_dsl import Index
 
-from etherscan_app.indexes import FolderDocument, AddressDocument
+from etherscan_app.indexes import FolderDocument
+from etherscan_app.models import Folder
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         index = Index(settings.ELASTICSEARCH_INDEX)
-        index.delete()
+        index.delete(ignore=404)
         index.document(FolderDocument)
-        index.document(AddressDocument)
         index.create()
+        [FolderDocument.from_folder(folder) for folder in Folder.objects.all()]
         self.stdout.write(self.style.SUCCESS(f"{index} index successfully created!"))
