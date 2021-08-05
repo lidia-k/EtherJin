6dce38adcef1e5ef04a66047d970c4098c08a69b
lidia-k <lidiakwon212@gmail.com>
2021-08-05 19:32:13 +0000

    change the show-transactions url to have the address param at the end; renamethe show-results url to create-alias

--- etherscan_app/urls.py
+++ etherscan_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search-results", views.search_results, name="search-results"),
     path("submit-address/<str:address>", views.submit_address, name="submit-address"),
-    path("results/<str:address>", views.show_results, name="results"),
+    path("create-alias/<str:address>", views.create_alias, name="create-alias"),
     path(
         "save-address-to-folder",
         views.save_address_to_folder,
@@ -22,7 +22,7 @@ urlpatterns = [
     ),
     path("folder/<str:folder_id>", views.show_folder, name="show-folder"),
     path(
-        "<str:address>/transactions", views.show_transactions, name="show-transactions"
+        "transactions/<str:address>", views.show_transactions, name="show-transactions"
     ),
     path("create-folder", views.create_folder, name="create-folder"),
     path("view-folders", views.show_folders, name="show-folders"),
